349c7fced04e8d9923248de075ed81df982d9cc0
tomast1337 <vycasnicolas@gmail.com>
2022-09-11 03:30:29 -0300

    Tilt hover attempt

--- src/components/stickerCard.tsx
+++ src/components/stickerCard.tsx
@@ -3,32 +3,96 @@ import style from "./stickerCard.module.scss";
 import { stickerType } from "../types/stickerType";
 
 export default (props: { sticker: stickerType }) => {
-    React.useEffect(() => {
-        //apply the animation
 
-    }, []);
+    const [tiltDeg, setTiltDeg] = React.useState({ x: 0, y: 0 });
+    const [lightPos, setLightPos] = React.useState({ x: 0, y: 0 });
+
+    const TiltEffect = (e: React.MouseEvent<HTMLDivElement>) => {
+        // get mouse position
+        const { left, top } = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - left;
+        const y = e.clientY - top;
+        // get width and height of element
+        const { offsetWidth: width, offsetHeight: height } = e.currentTarget;
+        // get the center of the div
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        const factorX = (centerX - x) / width * 2; // Clap the value between -1 and 1
+        const factorY = (centerY - y) / height * 2; // Clap the value between -1 and 1
+
+        const maxAngle = 360 / 20;
+        // set the angle
+        const degX = factorX * maxAngle;
+        const degY = factorY * maxAngle;
+
+        setTiltDeg({
+            x: degX,
+            y: degY
+        });
+
+        // update the spot light
+        const range = { max: 225, min: 50 };
+
+        const lightPosX = range.max + range.min * factorX;
+        const lightPosY = range.max + range.min * factorY;
+
+        console.log({ lightPosX, lightPosY });
+
+        setLightPos({
+            x: lightPosX,
+            y: lightPosY
+        });
+
+    };
+    const MouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+        setTiltDeg({ x: 0, y: 0 });
+        setLightPos({ x: 0, y: 0 });
+    };
 
     return (
-        <div className={style.card}>
-            <a href={
-                `https://steamcommunity.com/market/search?q=${props.sticker.name.replace(" ", "%20")}`
-            } target="_blank">
-                <h1>
-                    {props.sticker.name}
-                </h1>
-                <img src={props.sticker.image_url} alt={props.sticker.name} />
-                <div className={style.coloInfo}>
-                    <span style={{
-                        backgroundColor: props.sticker.dominant_colors[0],
-                    }} />
-                    <span style={{
-                        backgroundColor: props.sticker.dominant_colors[1],
-                    }} />
-                    <span style={{
-                        backgroundColor: props.sticker.dominant_colors[2],
-                    }} />
-                </div>
-            </a>
-        </div>
+        <>
+            <div className={style.card}
+                onMouseMove={TiltEffect}
+                onMouseLeave={MouseLeave}
+                style={{
+                    transform:
+                        `rotateX(${tiltDeg.y}deg)
+                 rotateY(${tiltDeg.x}deg)`,
+                }}>
+                <a href={
+                    `https://steamcommunity.com/market/search?q=${props.sticker.name.replace(" ", "%20")}`
+                } target="_blank">
+                    <h1>
+                        {props.sticker.name}
+                    </h1>
+
+                    <span
+                        className={style.spotLight}
+                        style={{
+                            transform: `translate(${lightPos.x}%, ${lightPos.y}%)`,
+                            display: lightPos.x === 0 && lightPos.y === 0 ? "none" : "inline-block",
+                            background: `radial-gradient(circle,
+                                rgba(255, 255, 255, ${0.1}) 0%,
+                                rgba(255, 255, 255, 0) 80%)`
+                        }} />
+
+                    <img
+                        src={props.sticker.image_url}
+                        alt={props.sticker.name} />
+                    <div className={style.coloInfo}>
+                        <span style={{
+                            backgroundColor: props.sticker.dominant_colors[0],
+                        }} />
+                        <span style={{
+                            backgroundColor: props.sticker.dominant_colors[1],
+                        }} />
+                        <span style={{
+                            backgroundColor: props.sticker.dominant_colors[2],
+                        }} />
+                    </div>
+                </a>
+            </div>
+        </>
     )
 }
